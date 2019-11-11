52a845469ef681f1aa486624167ee4197db127e8
SamerSenbol <samer.senbol@gmail.com>
2019-11-11 15:07:15 +0100

    Adding index.html file in the public folder and use this command: app.use(express.static('public'))
    for conecting all files which the public folder have to the index.js file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-app.set('view engine', 'ejs');
-
 const express = require('express')
 
 const app = express()
 const port = 3000
 
 app.get('/',(req, res)=> res.send('Hello World!'))
+
+app.use(express.static('public'))
+  
+
 app.listen(port,()=> console.log(`Example app listening on port ${port}!`))
